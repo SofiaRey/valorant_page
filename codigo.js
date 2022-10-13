c8c26e997b37bb81e18def29b65d6859da8c45b6
SofiaRey <sofia.rey.se@gmail.com>
2022-10-12 23:23:00 -0300

    feat: generar equipamiento logic finished

--- codigo.js
+++ codigo.js
@@ -1,7 +1,83 @@
 const endpointAgentes =
   "https://valorant-api.com/v1/agents?language=es-MX&isPlayableCharacter=true";
+const endpointMapas = "https://valorant-api.com/v1/maps?language=es-MX";
+const endpointArmas = "https://valorant-api.com/v1/weapons?language=es-MX";
+const endpointEscudos = "https://valorant-api.com/v1/gear?language=es-MX";
 let agentes;
-const selectAgente = document.querySelector("#agentes-select");
+let mapas;
+let armas;
+let escudos;
+const roles = [
+  {
+    nombre: "Iniciadores",
+    nombreApi: "Iniciador",
+    id: "1b47567f-8f7b-444b-aae3-b0c634622d10",
+  },
+  {
+    nombre: "Centinelas",
+    nombreApi: "Centinela",
+    id: "5fc02f99-4091-4486-a531-98459a3e95e9",
+  },
+  {
+    nombre: "Controladores",
+    nombreApi: "Controlador",
+    id: "4ee40330-ecdd-4f2f-98a8-eb1243428373",
+  },
+  {
+    nombre: "Duelistas",
+    nombreApi: "Duelista",
+    id: "dbe8757e-9e92-4ed4-b39f-9dfc589691d4",
+  },
+];
+
+// --------------- Obtener data inicial ---------------
+
+// --------------- Agentes
+fetch(endpointAgentes)
+  .then((response) => response.json())
+  .then((body) => {
+    agentes = body.data;
+
+    generarBotonesAspecto();
+
+    // Rellenar select de Generar equipamiento
+    agentes.map(
+      (e) =>
+        (selectAgente.innerHTML += `
+      <option value='${e.displayName}'>${e.displayName}</option>
+      `)
+    );
+
+    // Mostrar el primer agente de la lista por defecto en todas las pestañas
+    mostrarInfoAgenteRoles(roles[0].id);
+    mostrarInfoAgente(agentes[0].uuid);
+  });
+
+// --------------- Mapas
+fetch(endpointMapas)
+  .then((response) => response.json())
+  .then((body) => {
+    mapas = body.data;
+
+    // Rellenar select de Generar equipamiento
+    mapas.map(
+      (e) =>
+        (selectMapa.innerHTML += `
+      <option value='${e.displayName}'>${e.displayName}</option>
+      `)
+    );
+  });
+
+// --------------- Armas
+fetch(endpointArmas)
+  .then((res) => res.json())
+  .then((body) => (armas = body.data));
+
+// --------------- Escudos
+fetch(endpointEscudos)
+  .then((res) => res.json())
+  .then((body) => (escudos = body.data));
+
 // --------------- Navbar item activo ---------------
 const navItems = document.querySelectorAll(".nav-item");
 for (var i = 0; i < navItems.length; i++) {
@@ -12,7 +88,7 @@ for (var i = 0; i < navItems.length; i++) {
   });
 }
 
-// --------------- Cambiar entre modos de Exploración ---------------
+// --------------- Cambiar entre modos de Exploración de Agentes ---------------
 const selectorModo = document.querySelector("#explorar-modo");
 
 selectorModo.addEventListener("change", () => {
@@ -34,54 +110,35 @@ function cambiarModoExploracion() {
     case 1:
       document.getElementById("explorar-rol").classList.add("explorar-activo");
       break;
-    case 2:
-      document
-        .getElementById("explorar-nombre")
-        .classList.add("explorar-activo");
-      break;
   }
 }
-// --------------- Explorar por aspecto ---------------
 
+// --------------- Explorar por aspecto ---------------
 let contenedorAspecto = document.querySelector(".contenedor-aspecto");
 let contenedorAgente = document.querySelector(".contenedor-info-agente");
 let contenedorHabilidades = document.querySelector(".contenedor-habilidades");
 
-fetch(endpointAgentes)
-  .then((response) => {
-    return response.json();
-  })
-  .then((body) => {
-    agentes = body.data;
-    mostrarInfoAgenteRoles(roles[0].id);
-
-    agentes.map((e) => {
-      selectAgente.innerHTML += `
-      <option value='${e.displayName}'>${e.displayName}</option>
+function generarBotonesAspecto() {
+  agentes.map((e) => {
+    contenedorAspecto.innerHTML += `
+      <div id="${e.uuid}" class="borde-aspecto">
+        <img onclick="mostrarInfoAgente('${e.uuid}')" src="${e.displayIconSmall}" />
+      <div/>
       `;
-      contenedorAspecto.innerHTML += `
-        <div id="${e.uuid}" class="borde-aspecto">
-          <img onclick="mostrarInfoAgente('${e.uuid}')" src="${e.displayIconSmall}" />
-        <div/>
-        `;
-    });
-    mostrarInfoAgente(agentes[0].uuid);
   });
+}
 
 function mostrarInfoAgente(id) {
+  // Marcar aspecto seleccionado
   const aspectos = document.querySelectorAll(".borde-aspecto");
-  aspectos.forEach((aspecto) => {
-    aspecto.classList.remove("aspecto-activo");
-  });
-
+  aspectos.forEach((aspecto) => aspecto.classList.remove("aspecto-activo"));
   document.getElementById(id).classList.add("aspecto-activo");
 
+  // Traer información de agente seleccionado
   fetch(
     `https://valorant-api.com/v1/agents/${id}?language=es-MX&isPlayableCharacter=true`
   )
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((body) => {
       const agente = body.data;
       document.querySelector(".contenedor-info-agente > div > img").src =
@@ -93,6 +150,11 @@ function mostrarInfoAgente(id) {
         agente.role.displayName;
       document.getElementById("descripcion-agente").innerText =
         agente.description;
+
+      // Vaciar información de habilidad seleccionada previamente
+      document.querySelector(".contenedor-habilidad-info > h6").innerHTML = "";
+      document.querySelector(".contenedor-habilidad-info > p").innerHTML = "";
+
       generarHabilidades(agente.abilities);
     })
     .catch((e) => console.log(e));
@@ -113,54 +175,37 @@ function generarHabilidades(habilidades) {
 }
 
 function generarInfoHabilidad(nombre, descripcion, numero) {
+  // Marcar habilidad seleccionada
   const habilidades = document.querySelectorAll(".contenedor-habilidad");
-  habilidades.forEach((habilidad) => {
-    habilidad.classList.remove("habilidad-activa");
-  });
-
+  habilidades.forEach((habilidad) =>
+    habilidad.classList.remove("habilidad-activa")
+  );
   document.getElementById(numero).classList.add("habilidad-activa");
 
+  // Rellenar información
   document.querySelector(".contenedor-habilidad-info > h6").innerHTML = nombre;
   document.querySelector(".contenedor-habilidad-info > p").innerHTML =
     descripcion;
 }
 
 // --------------- Explorar por rol ---------------
-
-const roles = [
-  {
-    nombre: "Iniciadores",
-    id: "1b47567f-8f7b-444b-aae3-b0c634622d10",
-  },
-  {
-    nombre: "Centinelas",
-    id: "5fc02f99-4091-4486-a531-98459a3e95e9",
-  },
-  {
-    nombre: "Controladores",
-    id: "4ee40330-ecdd-4f2f-98a8-eb1243428373",
-  },
-  {
-    nombre: "Duelistas",
-    id: "dbe8757e-9e92-4ed4-b39f-9dfc589691d4",
-  },
-];
 const contenedorRoles = document.querySelector(".contenedor-roles");
 const contenedorAgentesRol = document.querySelector(".contenedor-agentes-rol");
 
+// Generar botones de roles
 roles.map((rol) => {
   contenedorRoles.innerHTML += `<div onclick="mostrarInfoAgenteRoles('${rol.id}')" class="boton-rol" id="${rol.id}"><p>${rol.nombre}</p></div>`;
 });
 
 function mostrarInfoAgenteRoles(id) {
+  // Marcar rol seleccionado
   const rolesBotones = document.querySelectorAll(".boton-rol");
-  rolesBotones.forEach((rol) => {
-    rol.classList.remove("rol-activo");
-  });
-
+  rolesBotones.forEach((rol) => rol.classList.remove("rol-activo"));
   document.getElementById(id).classList.add("rol-activo");
+
   contenedorAgentesRol.innerHTML = "";
 
+  // Mostrar agentes que coincidan con rol seleccionado
   agentes.map((agente) => {
     if (agente.role.uuid == id)
       contenedorAgentesRol.innerHTML += `
@@ -170,16 +215,6 @@ function mostrarInfoAgenteRoles(id) {
             </div>`;
   });
 }
-
-// --------------- Explorar por Nombre ---------------
-// const input = document.querySelector("#buscar-nombre");
-
-// input.addEventListener("input", (e) => {
-//   const text = event.target.value.trim();
-
-//   let agentesFiltrados = filtrarPorNombre(text, data);
-//   console.log(agentesFiltrados);
-// });
 
 // --------------- Generar Aleatorios ---------------
 
@@ -193,25 +228,47 @@ const contenedorAgentesAleatorios = document.querySelector(
   ".contenedor-agentes-aleatorios"
 );
 
-cantJugadoresSelect.addEventListener(
-  "input",
-  () => (cantJugadores = cantJugadoresSelect.value)
-);
-
 generarAleatoriosBoton.addEventListener("click", () => {
+  cantJugadores = cantJugadoresSelect.value;
   let agentesGenerados = [];
-  let agentesMezclados = mezclarArray(agentes);
+
   if (cantJugadores == 1) {
-    agentesGenerados = [agentesMezclados[0]];
+    // Usar un agente random cualquiera
+    agentesGenerados = [mezclarArray(agentes)[0]];
   } else {
+    // Randomizar los nombres de los roles
+    let rolesRandom = mezclarArray(roles.map((rol) => rol.nombreApi));
+
+    // Organizar los agentes segun los 4 roles
     const agentesPorRol = agentes.reduce((prev, act) => {
+      // A la array ya generada del rol actual (`prev["rolActual"]`), se la iguala a sí misma si existe
+      // Si no, iniciar una array nueva
+      // A la array resultante pushearle agente actual
+
+      // Simplificación ilustrativa:
+      // prev = {} // Valor inicial del `.reduce()`
+      // prev["rolActual"] = prev["rolActual"] != null ? prev["rolActual"] : []
+      // prev["rolActual"].push(agenteActual)
       (prev[act["role"]["displayName"]] =
-        prev[act["role"]["displayName"]] || []).push(act);
+        prev[act["role"]["displayName"]] ?? []).push(act);
       return prev;
     }, {});
 
+    // Generar un agente random sin repetir el rol por cada jugador
     for (let i = 0; i < cantJugadores; i++) {
-      agentesGenerados.push(agentesMezclados[i]);
+      if (i == 4) {
+        // Solo hay 4 roles, pero pueden ser 5 jugadores. El rol duplicado más recomendado es duelista.
+        agentesGenerados.push(agentesPorRol["Duelista"][0]);
+      } else {
+        // Index random dentro de la cantidad de agentes del rol que hay
+        let indexRandom = Math.floor(
+          Math.random() * agentesPorRol[rolesRandom[i]].length
+        );
+        agentesGenerados.push(agentesPorRol[rolesRandom[i]][indexRandom]);
+
+        // Remover agente usado para que no se repita
+        agentesPorRol[rolesRandom[i]].splice(indexRandom, 1);
+      }
     }
   }
 
@@ -230,19 +287,19 @@ const mostrarAgentesAleatorios = (agentesGenerados) => {
   });
 };
 
-function mezclarArray(arr) {
+function mezclarArray(array) {
   let random;
-  for (let actual = 0; actual < arr.length; actual++) {
+  for (let actual = 0; actual < array.length; actual++) {
     random = Math.floor(Math.random() * actual);
-    let temp = arr[actual];
-    arr[actual] = arr[random];
-    arr[random] = temp;
+    let temp = array[actual];
+    array[actual] = array[random];
+    array[random] = temp;
   }
-  return arr;
+  return array;
 }
 
 // --------------- Generar equipamiento ---------------
-
+const selectAgente = document.querySelector("#agentes-select");
 const selectMapa = document.querySelector("#mapa-select");
 const generarEquipBoton = document.querySelector("#generar-equip-boton");
 const inputDinero = document.querySelector("#input-dinero");
@@ -252,19 +309,22 @@ const contenedorEquipamiento = document.querySelector(
 const contenedorEquipamientoGenerado = document.querySelector(
   ".contenedor-equipamiento-generado"
 );
-let mapas;
 
 generarEquipBoton.addEventListener("click", () => {
+  // Validar input de dinero
   if (validarDinero()) {
     contenedorEquipamiento.classList.add("contenedor-equip-visible");
     contenedorEquipamientoGenerado.innerHTML = "";
 
+    // Resumen de parametros: dinero, agente seleccionado, mapa seleccionado
     equipamientoGen = generarEquipamiento(
       parseInt(inputDinero.value),
       agentes.find((agente) => agente.displayName == selectAgente.value),
-      "mapa"
+      mapas.find((mapa) => mapa.displayName == selectMapa.value)
     );
 
+    // --------------- Mostrar equipamiento generado
+    // Arma Principal
     if (equipamientoGen["armaPrincipal"] != null) {
       contenedorEquipamientoGenerado.innerHTML += `
         <div>
@@ -274,6 +334,7 @@ generarEquipBoton.addEventListener("click", () => {
         <h3>+</h3>
       `;
     }
+    // Arma Secundaria
     if (equipamientoGen["armaSecundaria"] != null) {
       contenedorEquipamientoGenerado.innerHTML += `
         <div>
@@ -283,6 +344,7 @@ generarEquipBoton.addEventListener("click", () => {
         <h3>+</h3>
     `;
     }
+    // Escudo
     if (equipamientoGen["escudo"] != null) {
       contenedorEquipamientoGenerado.innerHTML += `
         <div>
@@ -292,6 +354,7 @@ generarEquipBoton.addEventListener("click", () => {
         <h3>+</h3>
     `;
     }
+    // Habilidades (va siempre, porque es el dinero restante)
     contenedorEquipamientoGenerado.innerHTML += `
       <div>
         <h2>${equipamientoGen["habilidades"]}<h2/>
@@ -300,21 +363,6 @@ generarEquipBoton.addEventListener("click", () => {
     `;
   }
 });
-
-fetch("https://valorant-api.com/v1/maps?language=es-MX")
-  .then((response) => {
-    return response.json();
-  })
-  .then((body) => {
-    mapas = body.data;
-
-    mapas.map(
-      (e) =>
-        (selectMapa.innerHTML += `
-      <option value='${e.displayName}'>${e.displayName}</option>
-      `)
-    );
-  });
 
 function validarDinero() {
   const error = document.querySelector(".input-error");
@@ -341,28 +389,6 @@ function validarDinero() {
   return esValido;
 }
 
-// Obtener escudos
-let escudos;
-fetch("https://valorant-api.com/v1/gear?language=es-MX")
-  .then((res) => {
-    return res.json();
-  })
-  .then((body) => {
-    escudos = body.data;
-  });
-
-// Obtener armas
-let armas;
-fetch("https://valorant-api.com/v1/weapons?language=es-MX")
-  .then((res) => {
-    return res.json();
-  })
-  .then((body) => {
-    armas = body.data;
-  });
-
-// Prioridad: agente > mapa > rol
-
 function generarEquipamiento(dinero, agente, mapa) {
   const armaDefecto = armas.find((arma) => arma.displayName == "Classic");
   let equipamiento = {
@@ -373,12 +399,12 @@ function generarEquipamiento(dinero, agente, mapa) {
       precio: armaDefecto.shopData.cost,
     },
     escudo: null,
-    habilidades: null, // Las habilidades consisten en el dinero restante
+    habilidades: null, // Lo destinado para habilidades consiste en el dinero restante
   };
 
   function comprar(tipoArticulo, articulo) {
-    console.log("Articulo a comprar:");
-    console.log(articulo);
+    // tipoArticulo: armaPrimaria, armaSecundaria, escudo
+
     let dineroDisp = dinero;
 
     // En caso de que este sobreescribiendo y alcance el dinero con el reembolso para comprar
@@ -386,12 +412,15 @@ function generarEquipamiento(dinero, agente, mapa) {
       equipamiento[tipoArticulo] != null &&
       dinero + equipamiento[tipoArticulo].precio >= articulo.shopData.cost
     ) {
+      // Efectuar reembolso
       dineroDisp = dinero + equipamiento[tipoArticulo].precio;
     }
+
     if (dineroDisp < articulo.shopData.cost) return;
 
     dinero = dineroDisp;
 
+    // Rellenar o sobreescribir el tipo de equipamiento
     equipamiento[tipoArticulo] = {
       imagen: articulo.displayIcon,
       nombre: articulo.displayName,
@@ -403,6 +432,11 @@ function generarEquipamiento(dinero, agente, mapa) {
   }
 
   // Existen cuatro grados de equipamiento en función del dinero
+
+  // Dado a que las franjas son amplias, muchas veces se llama a comprar con el mismo tipo mas de una vez,
+  // ya que si no puede pagar cierto arma, quizá otro si, por lo que se van sobreesribiendo y 
+  // mejorando hasta es posible
+
   if (dinero <= 800) {
     switch (agente.role.displayName) {
       case "Iniciador":
@@ -427,19 +461,100 @@ function generarEquipamiento(dinero, agente, mapa) {
         );
         break;
     }
-
     if (agente.displayName == "Chamber") {
       comprar(
         "armaSecundaria",
         armas.find((arma) => arma.displayName == "Sheriff")
       );
     }
+
   } else if (dinero > 800 && dinero < 3000) {
-    console.log('entre 800 y 3000')
+    comprar("escudo", escudos[1]);
+    if (mapa.displayName != "Heaven" || mapa.displayName != "Bind")
+      comprar("escudo", escudos[0]);
+    switch (agente.role.displayName) {
+      case "Iniciador":
+        comprar("escudo", escudos[1]);
+        if (agente.displayName == "Fade") {
+          comprar(
+            "armaPrincipal",
+            armas.find((arma) => arma.displayName == "Bucky")
+          );
+        } else {
+          comprar(
+            "armaPrincipal",
+            armas.find((arma) => arma.displayName == "Bulldog")
+          );
+        }
+        comprar(
+          "armaSecundaria",
+          armas.find((arma) => arma.displayName == "Frenzy")
+        );
+        break;
+      case "Centinela":
+        if (agente.displayName == "Chamber") {
+          comprar(
+            "armaPrincipal",
+            armas.find((arma) => arma.displayName == "Marshal")
+          );
+        }
+        comprar(
+          "armaSecundaria",
+          armas.find((arma) => arma.displayName == "Frenzy")
+        );
+        comprar("escudo", escudos[1]);
+        break;
+      case "Duelista":
+        comprar(
+          "armaSecundaria",
+          armas.find((arma) => arma.displayName == "Frenzy")
+        );
+        comprar(
+          "armaSecundaria",
+          armas.find((arma) => arma.displayName == "Sheriff")
+        );
+        break;
+      case "Controlador":
+        comprar(
+          "armaSecundaria",
+          armas.find((arma) => arma.displayName == "Frenzy")
+        );
+        comprar(
+          "armaSecundaria",
+          armas.find((arma) => arma.displayName == "Sheriff")
+        );
+        break;
+    }
+
   } else if (3000 < dinero && dinero < 6000) {
-    console.log('entre 3000 y 6000')
+    comprar("escudo", escudos[0]);
+    if (agente.displayName == "Breach" && mapa.displayName == "Odin") {
+      comprar(
+        "armaPrincipal",
+        armas.find((arma) => arma.displayName == "Odin")
+      );
+    }
+    if (
+      agente.displayName == "Raze" ||
+      mapa.displayName == "Pearl" ||
+      mapa.displayName == "Fracture"
+    ) {
+      comprar(
+        "armaPrincipal",
+        armas.find((arma) => arma.displayName == "Phanton")
+      );
+    } else {
+      comprar(
+        "armaPrincipal",
+        armas.find((arma) => arma.displayName == "Vandal")
+      );
+    }
+
+    comprar(
+      "armaPrincipal",
+      armas.find((arma) => arma.displayName == "Spectre")
+    );
   } else if (dinero >= 6000) {
-    console.log('mas de 6000')
     comprar("escudo", escudos[0]);
 
     comprar(
@@ -455,43 +570,6 @@ function generarEquipamiento(dinero, agente, mapa) {
     }
   }
 
-  console.log("Equipamiento final:");
   equipamiento["habilidades"] = dinero;
-  console.log(equipamiento);
   return equipamiento;
 }
-
-// plata < 800
-
-// Chamber sherif
-// Controladores shorty
-// Duelistas frenzy
-// Iniciadores escudo ligero + classic
-// Centinelas ghost + escudo
-// Icebox classic
-
-// 800 < plata < 3000
-
-// Escudo pesado
-// Chamber Marshal
-// Brimstone judge
-// Duelistas y Controladores sherif
-// Centinelas Guardian
-// Iniciadores Bulldog y escudo ligero
-// Heaven, Bind escudo ligero
-
-// 3000 < plata > 5000
-
-// Raze Phantom
-// Menos por Raze, todos Vandal
-// Breach Ascent Odin
-// Todos escudo pesado
-// Pearl y Fracture todos Phantom
-
-// plata > 5000
-
-// Chamber sherif y operator
-// Todos escudo pesado
-// Jett Icebox operator
-// Viper shorty
-// Bind todos frenzy
